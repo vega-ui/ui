3dd046317b6331dc674a29a73fbcb8a39872b1f9
SlDo <slavadodonov19@gmail.com>
2024-12-20 02:54:26 +0300

    feat: 🎸 added menu-left icon

--- src/components/Icon/utils/getIcon/getIcon.ts
+++ src/components/Icon/utils/getIcon/getIcon.ts
@@ -1,11 +1,13 @@
 import SupportIcon from '../../../../assets/icons/support.svg?react'
 import GlobeIcon from '../../../../assets/icons/globe.svg?react'
 import ChevronIcon from '../../../../assets/icons/chevron.svg?react'
+import MenuLeftIcon from '../../../../assets/icons/menu-left.svg?react'
 
 const icons = {
   support: SupportIcon,
   globe: GlobeIcon,
-  chevron: ChevronIcon
+  chevron: ChevronIcon,
+  menuLeft: MenuLeftIcon
 } as const
 
 export type IconName = keyof typeof icons
