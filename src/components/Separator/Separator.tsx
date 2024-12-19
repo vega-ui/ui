60b48fab763aff0389e80e6a0986e30811d57781
SlDo <slavadodonov19@gmail.com>
2024-12-20 01:26:15 +0300

    feat: 🎸 added vertical orientation for separator

--- src/components/Separator/Separator.tsx
+++ src/components/Separator/Separator.tsx
@@ -5,10 +5,11 @@ import { csx } from '../../utils/css';
 
 export interface SeparatorProps {
   className?: string
+  orientation?: 'horizontal' | 'vertical'
 }
 
-export const Separator: FC<SeparatorProps> = ({ className }) => {
+export const Separator: FC<SeparatorProps> = ({ orientation = 'horizontal', className }) => {
   return (
-    <div role='separator' className={csx(style.separator, className)} />
+    <div role='separator' aria-orientation={orientation} data-orientation={orientation} className={csx(style.separator, className)} />
   );
 }
