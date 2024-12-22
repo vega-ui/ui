6da62cab145132f126a0d24e27d3beae5bedb78f
SlDo <slavadodonov19@gmail.com>
2024-12-23 02:53:49 +0300

    feat: 🎸 changed font size for clamped text

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -9,7 +9,7 @@ type HeadingAs = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 export interface HeadingProps<T extends HeadingAs = 'h1'> {
   className?: string
   as?: T
-  children?: string
+  children?: string | string[]
   size?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11
   fontWeight?: 400 | 500 | 700 | 900
 }
