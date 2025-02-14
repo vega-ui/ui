68dc0da94c2e89f77c856e7e8c3cd6ed7ce63173
Slava Dodonov <slavadodonov19@gmail.com>
2025-02-14 04:39:42 +0300

    feat: 🎸 addex export of ResponsiveModalHeader

--- packages/responsive-ui/src/components/ResponsiveModal/index.ts
+++ packages/responsive-ui/src/components/ResponsiveModal/index.ts
@@ -1,2 +1,3 @@
 export { ResponsiveModal, type ResponsiveModalProps } from './ResponsiveModal.tsx'
+export { ResponsiveModalHeader, type ResponsiveModalHeaderProps } from './components'
 export { useResponsiveModalContext } from './hooks'
