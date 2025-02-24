530a18621d2faab599146e986fa29fe83be30394
Slava Dodonov <slavadodonov19@gmail.com>
2025-02-24 03:51:51 +0300

    reverted story

--- packages/ui/src/components/NumberField/NumberField.stories.tsx
+++ packages/ui/src/components/NumberField/NumberField.stories.tsx
@@ -23,9 +23,7 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Default: Story = {
   args: {
-    placeholder: 'Количество',
-    max: 16,
-    min: 1,
+    placeholder: 'Количество'
   },
 };
 
