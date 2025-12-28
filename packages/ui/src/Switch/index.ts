cfba4930e18e883ed5567e6e355a3e307a9f5e90
Slava Dodonov <slavadodonov19@gmail.com>
2025-12-28 04:53:59 +0300

    fix: 🐛 added useSwitchContext to import

--- packages/ui/src/Switch/index.ts
+++ packages/ui/src/Switch/index.ts
@@ -3,4 +3,5 @@ export {
   SwitchHiddenInput, type SwitchHiddenInputProps,
   SwitchIndicator, type SwitchIndicatorProps
 } from './components'
-export type { SwitchVariant, SwitchSize } from './types.ts'
+export type { SwitchVariant, SwitchSize } from './types'
+export { useSwitchContext } from './contexts'
